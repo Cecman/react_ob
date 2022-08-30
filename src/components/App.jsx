5f5f54420776c8904f0c061314336d8638572fc2
Cecman <jane.trajkoski@agiledrop.com>
2022-08-30 12:03:04 +0200

    Removed unused code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,31 +1,7 @@
 import React from 'react';
 import MainMenuLink from './Atoms/MainMenuLink/MainMenuLink';
-// import MenuLinkList from './Molecules/MenuLinkList/MenuLinkList';
 
 function App() {
-  // const linkArray = [
-  //   {
-  //     key: 1,
-  //     label: 'Popular',
-  //     url: '/movie',
-  //   },
-  //   {
-  //     key: 2,
-  //     label: 'Now playing',
-  //     url: '/movie/now-playing',
-  //   },
-  //   {
-  //     key: 3,
-  //     label: 'Upcoming',
-  //     url: '/movie/upcoming',
-  //   },
-  //   {
-  //     key: 4,
-  //     label: 'Top rated',
-  //     url: '/movie/top-rated',
-  //   },
-  // ];
-
   return <MainMenuLink label="Movies" />;
 }
 
